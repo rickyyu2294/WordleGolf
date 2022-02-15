f3e4870440e1430e0dfb0e98e006e8544938a85d
Ricky Yu <rickyyu2294@gmail.com>
2022-02-15 00:29:08 -0800

    first commit

---
+++ database.py
@@ -0,0 +1,106 @@
+import sqlite3
+from sqlite3 import Error
+from unicodedata import name
+
+def initialize_database(db_file):
+    sql_create_users_table = """CREATE TABLE IF NOT EXISTS users (
+                                    id integer PRIMARY KEY,
+                                    name text NOT NULL UNIQUE
+                                );"""
+    
+    sql_create_scores_table =    """CREATE TABLE IF NOT EXISTS scores (
+                                        id integer PRIMARY KEY,
+                                        score integer NOT NULL,
+                                        puzzle_id integer NOT NULL,
+                                        user_id integer NOT NULL
+                                    );"""
+
+    conn = create_connection(db_file)
+
+    if conn is not None:
+        create_table(conn, sql_create_users_table)
+        create_table(conn, sql_create_scores_table)
+        conn.close()
+    else:
+        print("Error! cannot create the database connection")
+
+def insert_user(db_file, name):
+    conn = create_connection(db_file)
+    sql =    """INSERT INTO users(name) VALUES(?)"""
+
+    if conn is not None:
+        cur = conn.cursor()
+        cur.execute(sql, (name,))
+        conn.commit()
+    else:
+        print("Error! cannot create the database connection")
+
+    conn.close()
+
+    return cur.lastrowid
+
+def add_score(db_file, user_id, puzzle_id, score):
+    conn = create_connection(db_file)
+    sql =    """INSERT INTO scores(score, puzzle_id, user_id) VALUES(?, ?, ?)"""
+
+    if conn is not None:
+        cur = conn.cursor()
+        cur.execute(sql, (score, puzzle_id, user_id))
+        conn.commit()
+    else:
+        print("Error! cannot create the database connection")
+
+    conn.close()
+
+    return cur.lastrowid
+
+def select_user_id(db_file, name):
+    conn = create_connection(db_file)
+    sql = "SELECT id FROM users WHERE name=?"
+    cur = conn.cursor()
+    cur.execute(sql, (name,))
+    rows = cur.fetchall()
+
+    conn.close()
+    print(rows)
+    if len(rows) == 0:
+        print("None")
+        return None
+    else:
+        print(rows[0][0])
+        return rows[0][0]
+    
+def select_user_score(db_file, user_id, puzzle_id):
+    conn = create_connection(db_file)
+    sql = "SELECT score FROM scores WHERE (user_id=? and puzzle_id=?)"
+    cur = conn.cursor()
+    cur.execute(sql, (user_id, puzzle_id))
+    rows = cur.fetchall()
+
+    conn.close()
+    print("SCORES QUERY")
+    print(rows)
+    if len(rows) == 0:
+        print("None")
+        return None
+    else:
+        print(rows[0][0])
+        return rows[0][0]
+
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+    except Error as e:
+        print(e)
+
+    return conn
+
+def create_table(conn, create_table_sql):
+    try:
+        c = conn.cursor()
+        c.execute(create_table_sql)
+    except Error as e:
+        print(e)
+    
---
+++ main.py
@@ -0,0 +1,125 @@
+import discord
+import os
+import re
+import random
+import asyncio
+from datetime import date
+from datetime import datetime
+import pytz
+from discord.ext import commands, tasks
+import time
+import threading
+import database
+import json
+f = open('config.json')
+data = json.load(f)
+
+# constants and literals
+MAX_TRIES = 6
+BOT_CHANNEL_ID = 879170130125414450
+BOT_TOKEN = data["token"]
+PST = pytz.timezone('US/Pacific')
+DB_FILE = r"sqliteDatabase.db"
+client = discord.Client()
+
+#def delete(name):
+#  match = db.prefix(name)
+  #userScores = db[name];
+  #if len(match) != 0:
+  #  del db[name]
+  #  print("Deleted " + name)
+  #else:
+  #  print("No entry for " + name)
+
+# ============================
+# Events
+# ============================
+
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
+  print('v0.03')
+  database.initialize_database(DB_FILE)
+
+  await midnight()
+
+async def midnight():
+  now = datetime.now(tz=PST)
+  targetTime = datetime(now.year, now.month, now.day + 1, 0, 0, 0, 0, PST)
+  diff = (targetTime - now).total_seconds()
+  await asyncio.sleep(diff)
+  channel = client.get_channel(BOT_CHANNEL_ID)
+  await channel.send("Is it midnight?")
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  name = str(message.author) 
+  today = date.today()
+
+  if message.content.startswith('$hello'):
+    print('moo')
+    await message.channel.send('hi')
+
+  # if message.content.startswith('$delete'):
+  #   delete(name)
+
+  # CHECK SCORE ====================================================
+  # check daily and weekly score and print both in channel
+  if message.content.startswith('$score'):
+    print("Today's date:", today)
+    # dummy messages
+    await message.channel.send("Stats for " + name)
+    await message.channel.send("Today's score is: " + "[score]")
+    await message.channel.send("Total points for the week: " + "[scoreSum]")
+
+    # if played today, return daily score
+    # print("Your score today is ")
+    # else, notify that they need to play today
+
+    # additionally, attempt to return running sum for weekly score
+    # if sum does not exist, notify they need to participate once?
+    # (or, default to giving them 6 pts per missed day?)
+
+  # / CHECK SCORE ==================================================
+
+  # Match Wordle Message
+  pattern = re.compile("Wordle [0-9].. [0-9]/[0-9]")
+  regex_match = re.match(pattern, message.content)
+
+  if regex_match:
+    s = message.content.split()
+    name = str(message.author)
+    puzzle_id = s[1]
+    score = s[2].split("/")[0]
+    addScore(name, puzzle_id, score)
+
+def addScore(name, puzzle_id, score):
+  # add new score to user's score list
+  # check if user is in database
+  user_id = database.select_user_id(DB_FILE, name)
+  print("ADDSCORE")
+  print("User_ID " + str(user_id))
+  if user_id is not None:
+    score_id = database.select_user_score(DB_FILE, user_id, puzzle_id)
+    print("SCORE_ID " + str(score_id))
+    if score_id is None:
+      print("ADDING SCORE FOR EXISTING USER")
+      database.add_score(DB_FILE, user_id, puzzle_id, score)
+    else:
+      print("SCORE FOR DAY ALREADY EXISTS")
+      return
+
+  # else, add new user to db
+  else:
+    user_id = database.insert_user(DB_FILE, name)
+    database.add_score(DB_FILE, user_id, puzzle_id, score)
+    print("ADDING NEW USER " + name)
+  
+  temp = (name + " scored " + score + " on day " + puzzle_id)
+  print(temp)
+
+# Execute
+client.run(BOT_TOKEN)
