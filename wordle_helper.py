c73b2e20beb60da6f507b470924106d469e5474f
Ricky Yu <rickyyu2294@gmail.com>
2022-02-20 11:50:14 -0800

    Add helper function for numeric reactions

--- wordle_helper.py
+++ wordle_helper.py
@@ -1,5 +1,6 @@
 from datetime import date, timedelta
 from datetime import datetime
+import discord
 
 BASE_WORDLE_ID_TO_DATE = (244, datetime(2022, 2, 18))
 
@@ -16,3 +17,29 @@ def puzzle_id_to_date(puzzle_id):
     return None
   diff = puzzle_id - BASE_WORDLE_ID_TO_DATE[0]
   return BASE_WORDLE_ID_TO_DATE[1] + timedelta(days=diff)
+
+async def numeric_reaction(message, num):
+    for e in str(num):
+        print(e)
+        if e == '0':
+            await message.add_reaction("0️⃣")
+        elif e == '1':
+            await message.add_reaction("1️⃣")
+        elif e == '2':
+            await message.add_reaction("2️⃣")
+        elif e == '3':
+            await message.add_reaction("3️⃣")
+        elif e == '4':
+            await message.add_reaction("4️⃣")
+        elif e == '5':
+            await message.add_reaction("5️⃣")
+        elif e == '6':
+            await message.add_reaction("6️⃣")
+        elif e == '7':
+            await message.add_reaction("7️⃣")
+        elif e == '8':
+            await message.add_reaction("8️⃣")
+        elif e == '9':
+            await message.add_reaction("9️⃣")
+
+    return
