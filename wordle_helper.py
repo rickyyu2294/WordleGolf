ba5240eb8f403e4bbc3deed791a92205c604b9d7
Ricky Yu <rickyyu2294@gmail.com>
2022-02-20 23:49:10 -0800

    Add PST timezone to helper functions

--- wordle_helper.py
+++ wordle_helper.py
@@ -6,18 +6,20 @@ PST = pytz.timezone('US/Pacific')
 BASE_WORDLE_ID_TO_DATE = (244, datetime(2022, 2, 18).astimezone(PST))
 
 def date_to_puzzle_id(date):
-  delta = date - BASE_WORDLE_ID_TO_DATE[1]
-  delta_days = delta.days
-  return_id = BASE_WORDLE_ID_TO_DATE[0] + delta_days
-  if return_id > 0:
-    return return_id
+    date.astimezone(PST)
+    delta = date - BASE_WORDLE_ID_TO_DATE[1]
+    delta_days = delta.days
+    return_id = BASE_WORDLE_ID_TO_DATE[0] + delta_days
+    if return_id > 0:
+        return return_id
 
 def puzzle_id_to_date(puzzle_id):
-  # get diff b/t base and requested puzzle_id
-  if puzzle_id <= 0:
-    return None
-  diff = puzzle_id - BASE_WORDLE_ID_TO_DATE[0]
-  return BASE_WORDLE_ID_TO_DATE[1] + timedelta(days=diff)
+    # get diff b/t base and requested puzzle_id
+    if puzzle_id <= 0:
+        return None
+    diff = puzzle_id - BASE_WORDLE_ID_TO_DATE[0]
+    date = BASE_WORDLE_ID_TO_DATE[1] + timedelta(days=diff)
+    return date.astimezone(PST)
 
 async def numeric_reaction(message, num):
     for e in str(num):
